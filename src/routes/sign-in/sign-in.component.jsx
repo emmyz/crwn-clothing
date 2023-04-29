a7b0114d6a6619750feed3e68051b2a74359885c
Emmy Zhang <emmyz0114@gmail.com>
2023-04-28 17:42:53 -0700

    added firebase and utils, implemented GoogleAuth in SignIn Component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,18 @@
+import { Fragment } from 'react';
+import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 
+const logGoogleUser = async ()=>{
+    const { user } = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user);
+}
 const SignIn = () => {
     return (
-        <div>Sign In Page</div>
+        <Fragment>
+            <div>Sign In Page</div>
+            <button onClick={logGoogleUser}>Sign In With Google</button>
+
+        </Fragment>
+        
     );
 }
 
